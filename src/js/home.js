403ed9883c4d10dbfadb4ed13d5e03ae6864acfc
Jarvis Murray <jarvis011877@gmail.com>
2025-06-29 20:06:04 -0400

    Last style changes

--- src/js/home.js
+++ src/js/home.js
@@ -77,5 +77,5 @@ document.addEventListener("DOMContentLoaded", function () {
 // Logout
 function logout() {
 alert("Logging out...");
-window.location.href = "index.html/";
+window.location.href = "../../index.html";
 } 
