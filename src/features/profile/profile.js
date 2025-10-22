05b5f2d06bec9265c761e8b6506f93e086ebbc3f
Siddhant Raghuwanshi <siddhantraghuwanshi99@gmail.com>
2025-10-22 18:15:16 +0530

    Profile page: Custom Dropdown Multi Select

--- src/features/profile/profile.js
+++ src/features/profile/profile.js
@@ -313,7 +313,34 @@ function loadProfile() {
       calibrationBtn.textContent = "Submit";
     }
   });
+const customSelect = document.getElementById('sportsCustom');
+  const optionsList = document.getElementById('sportsOptions');
+  const checkboxes = optionsList.querySelectorAll('input[type="checkbox"]');
+  const mainBox = document.querySelector('.main-box'); // Adjust if needed
+  const options=document.querySelectorAll('#sportsOptions label')
 
+  // Toggle dropdown
+  customSelect.addEventListener('click', () => {
+    optionsList.classList.toggle('hidden');
+    mainBox.classList.toggle('extended-main-box')
+  });
+  
+  // Update selected options in select box
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      const selected = [];
+      checkboxes.forEach(cb => {
+        if (cb.checked) {
+          selected.push(cb.value);
+          // options.classList.toggle('selected')
+        }
+      });
+
+      customSelect.textContent = selected.length > 0
+        ? selected.join(', ')
+        : 'Select Sport';
+    });
+  });
 
 }
 
